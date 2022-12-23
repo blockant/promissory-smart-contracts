3c39275a7e214d62ceda4ac549b07cc522165710
Akshay Jain <akshayjain1999.aj18@gmail.com>
2022-12-23 20:44:15 +0530

    dummy-commit

---
+++ hardhat.config.js
@@ -0,0 +1,16 @@
+require("dotenv").config()
+
+/** @type import('hardhat/config').HardhatUserConfig */
+const GOERLI_RPC_URL =process.env.GOERLI_RPC_URL
+const PRIVATE_KEY =process.env.PRIVATE_KEY 
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    hardhat: {},
+    // goerli: {
+    //   url: GOERLI_RPC_URL,
+    //   accounts: [PRIVATE_KEY],
+    //   chainId: 5,
+    // }
+  },
+};
---
+++ scripts/deploy.js
@@ -0,0 +1,12 @@
+const { ethers, run, network } = require("hardhat")
+
+async function main(){
+
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
