848b6bf5ad8baf03784e2fe4b8375964a9199e88
Akshay Jain <akshayjain1999.aj18@gmail.com>
2022-12-27 00:15:27 +0530

    feat(test): Added basic property Addition Test Cases

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,7 +3,7 @@ const hre = require("hardhat");
 async function main() {
 
   const PromissoryContract = await hre.ethers.getContractFactory("Promissory");
-  const promissory = await PromissoryContract.deploy("0x2F13629e03286fA8C1135AfBccaF7DF810299fC4", "0x40154F00f2a6c0Ef7b7A2589c55D7250b43861d6", "0xdEf69C61a516E2648ABeED821a25422e4D0E91F6", "0xdFDBc88Ca3A8D6E4D3e6db389BE1baAA88f2dfC3");
+  const promissory = await PromissoryContract.deploy("0x2F13629e03286fA8C1135AfBccaF7DF810299fC4", "0x40154F00f2a6c0Ef7b7A2589c55D7250b43861d6");
 
   await promissory.deployed();
 
