0f1c7e170d9ed69b8da7e47a70944c4bfa05117e
Akshay Jain <akshayjain1999.aj18@gmail.com>
2022-12-27 01:41:13 +0530

    enhancement(Basic Test): Tests template added

--- test/Promissory.js
+++ test/Promissory.js
@@ -1,27 +1,69 @@
 const {expect}=require('chai')
 const hre=require('hardhat')
+const tokenName="Promissory Token"
+const propertyTokenSymbol="XYZ"
 describe("Promissory Contract", ()=>{
     let PromissoryContract, promissory 
-    beforeEach(async ()=>{
+    before(async ()=>{
         PromissoryContract = await hre.ethers.getContractFactory("Promissory");
         promissory =  await PromissoryContract.deploy("0x2F13629e03286fA8C1135AfBccaF7DF810299fC4", "0x40154F00f2a6c0Ef7b7A2589c55D7250b43861d6");
         await promissory.deployed();
         console.log(`Promissory Contract Deployed ${promissory.address}`)
     })
-
-    describe("Add Property", ()=>{
+    describe("Add Property", ()=>{  
         it("It should Add Property with all valid details", async ()=>{
-            await expect(promissory.addProperty("Promissory Token", "XYZ", 100,725, 1 )).to.emit(promissory, "PropertyAdded") 
+            await expect(promissory.addProperty(tokenName, propertyTokenSymbol, 100,725, 1 )).to.emit(promissory, "PropertyAdded") 
         })
         it("Add Property should Throw Error if token supply is negative", async ()=>{
-            await expect(promissory.addProperty("Promissory Token", "XYZ", 100,-725, 1 )).to.be.reverted
+            await expect(promissory.addProperty(tokenName, propertyTokenSymbol, -100,725, 1 )).to.Throw
         })
-        // it("Add Property should throw error if interest rate is negative", async ()=>{
-        //     await expect(promissory.addProperty("Promissory Token", "XYZ", 100,7.25, 1 )).to.be.reverted
-        // })
-        // it("Add Property should throw error if locking period is negative", async ()=>{
-        //     await expect(promissory.addProperty("Promissory Token", "XYZ", 100,7.25, 1 )).to.be.reverted
-        // })
+        it("Add Property should throw error if interest rate is negative", async ()=>{
+            await expect(promissory.addProperty(tokenName, propertyTokenSymbol, 100,-725, 1 )).to.Throw
+        })
+        it("Add Property should throw error if locking period is negative", async ()=>{
+            await expect(promissory.addProperty(tokenName, propertyTokenSymbol, 100,725, -1 )).to.Throw
+        })
+        it("Add Property should throw error if locking period is negative", async ()=>{
+            await expect(promissory.addProperty(tokenName, propertyTokenSymbol, 100,725, -1 )).to.Throw
+        })
+        it('List of properties added should be 1', async()=>{
+            const response=(await promissory.property(0))
+            console.log('Response is', response)
+            //expect(response).to
+        })
+    })
+    describe("Update Interest Rate of Property", ()=>{  
+        it("It should Throw Error if SM caller is not Owner", async ()=>{
+            await expect((await promissory.updateInterestRate(1, 800))).to.Throw 
+        })
+        it("It should Update Interest Rate of property if SM caller is Owner", async ()=>{
+            //console.log('Promisoory is', promissory)
+            await expect(await promissory.updateInterestRate(1, 800).call({from: "0x2F13629e03286fA8C1135AfBccaF7DF810299fC4"})).to.emit(promissory, "InterestRateUpdated") 
+        })
+    })
+    describe("Approve Property", ()=>{  
+        it("It should Throw Error if SM caller is not Owner", async ()=>{
+            await expect((await promissory.approveProperty(1, 800))).to.Throw 
+        })
+        it("It should Throw Error if SM caller is Owner, but property Id is invalid", async ()=>{
+            await expect((await promissory.approveProperty(1, 800))).to.Throw 
+        })
+        it("It should Approve Property if SM caller is Owner", async ()=>{
+            //console.log('Promisoory is', promissory)
+            await expect(await promissory.updateInterestRate(1, 800).call({from: "0x2F13629e03286fA8C1135AfBccaF7DF810299fC4"})).to.emit(promissory, "InterestRateUpdated") 
+        })
+    })
+    describe("Ban Property", ()=>{  
+        it("It should Throw Error if SM caller is not Owner", async ()=>{
+            await expect((await promissory.approveProperty(1, 800))).to.Throw 
+        })
+        it("It should Throw Error if SM caller is Owner, but property Id is invalid", async ()=>{
+            await expect((await promissory.approveProperty(1, 800))).to.Throw 
+        })
+        it("It should Approve Property if SM caller is Owner", async ()=>{
+            //console.log('Promisoory is', promissory)
+            await expect(await promissory.updateInterestRate(1, 800).call({from: "0x2F13629e03286fA8C1135AfBccaF7DF810299fC4"})).to.emit(promissory, "InterestRateUpdated") 
+        })
     })
     
 })
