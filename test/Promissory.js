a0d8a9ca59181b9c858f68b1f434106f33c15827
Akshay Jain <akshayjain1999.aj18@gmail.com>
2023-01-06 16:28:56 +0530

    feat(tests): Added Investment Tests

--- test/Promissory.js
+++ test/Promissory.js
@@ -3,10 +3,10 @@ const hre=require('hardhat')
 const tokenName="Promissory Token"
 const propertyTokenSymbol="XYZ"
 describe("Promissory Contract", ()=>{
-    let PromissoryContract, promissory , owner, promissoryUser2, promissoryUser1, user1PropertyId, ownerPropertyId
+    let PromissoryContract, promissory , owner, promissoryUser2, promissoryUser1,promissoryUser3, user1PropertyId, ownerPropertyId, contract
     
     before(async ()=>{
-        [owner, promissoryUser1,promissoryUser2 ] = await ethers.getSigners()
+        [owner, promissoryUser1,promissoryUser2, promissoryUser3] = await ethers.getSigners()
         console.log('Deployer/owner address is', owner.address)
         console.log('Promissory User 1 Address', promissoryUser1.address)
         console.log('Promissory User 2 Address', promissoryUser2.address)
@@ -86,7 +86,6 @@ describe("Promissory Contract", ()=>{
             await expect(promissory.approveProperty(1, 100)).to.be.reverted
         })
     })
-    //TODO: Should interest Rate Be Updated if Property is approved?
     describe("Update Interest Rate of Property", ()=>{  
         it("If Property Updater is not promissory owner it should throw error", async ()=>{
             await expect((promissory.connect(promissoryUser1).updateInterestRate(1, 2))).to.be.revertedWith('Caller is not the owner of the platform') 
@@ -100,8 +99,6 @@ describe("Promissory Contract", ()=>{
             await expect(promissory.updateInterestRate(2, 3)).to.be.revertedWith('Property isn\'t approved yet!')
         })
     })
-    //TODO: Can Property Be banned if approoved
-    //TODO: Can Banned Property be aprooved?
     describe("Ban Property", ()=>{  
         it("If Property Banner is not promissory owner it should throw error", async ()=>{
             await expect((promissory.connect(promissoryUser1).banProperty(1))).to.be.revertedWith('Caller is not the owner of the platform') 
@@ -114,8 +111,6 @@ describe("Promissory Contract", ()=>{
             await expect(promissory.banProperty(10)).to.be.revertedWith('Property do not exist!!')
         })
     })
-    //TODO: Can property Owner invest in his own property?
-    //TODO: Can Investor re-invest in same property?
     describe("Invest In Property", ()=>{  
         it("User should Invest in Approved Property", async ()=>{
             await expect((promissory.connect(promissoryUser1).investInProperty(1, 10))).to.emit(promissory, 'Invested') 
@@ -123,12 +118,43 @@ describe("Promissory Contract", ()=>{
         it("Limit should be changed from 100 to 90, thus betting 100 should throw error", async ()=>{
             await expect((promissory.connect(promissoryUser1).investInProperty(1, 100))).to.be.revertedWith('Invested Amount exceeds the number of Property Tokens available') 
         })
-        it("It should Throw Error if Property is banned", async ()=>{
-            await expect((promissory.investInProperty(2,10))).to.be.revertedWith('Property isn\'t approved yet!, Wait for platform to approve this property.') 
+        it("It should Invest In Property with another Limit", async ()=>{
+            await expect((promissory.connect(promissoryUser1).investInProperty(1,20))).to.be.emit(promissory, 'Invested')
         })
-        it("It should Approve Property if SM caller is Owner", async ()=>{
-            //console.log('Promisoory is', promissory)
-            await expect(promissory.updateInterestRate(1, 800).call({from: "0x2F13629e03286fA8C1135AfBccaF7DF810299fC4"})).to.emit(promissory, "InterestRateUpdated") 
+        it("User other than property owner can too invest in the property", async ()=>{
+            await expect((promissory.connect(promissoryUser2).investInProperty(1,30))).to.be.emit(promissory, 'Invested')
+        })
+        it("It should Throw Error if property is banned", async ()=>{
+            await expect((promissory.connect(promissoryUser1).investInProperty(1,20))).to.be.revertedWith('Property isn\'t approved yet!, Wait for platform to approve this property.')
+        })
+    })
+    //TODO: Can a property be banned after investment?
+    describe("Claim Investement", ()=>{  
+        it("Should throw an error, if user claiming investment is not property owner", async ()=>{
+            await expect((promissory.connect(promissoryUser2).claimInvestment(1, 10))).to.be.revertedWith('You are not the onwer of this property!')
+        })
+        it("Should throw an error, if property owner claiming investment tries to claim more than available", async ()=>{
+            await expect((promissory.connect(promissoryUser1).claimInvestment(1, 50))).to.be.revertedWith('Amount exceeds than available!')
+        })
+        it("User should be able to claim, the investment partially", async ()=>{
+            await expect((promissory.connect(promissoryUser1).investInProperty(1,10))).to.be.emit(promissory, 'ClaimedInvestment')
+        })
+        it("User should be able to claim, the investment completely", async ()=>{
+            await expect((promissory.connect(promissoryUser1).investInProperty(1,20))).to.be.emit(promissory, 'ClaimedInvestment')
+        })
+    })
+    describe("Claim Return", ()=>{  
+        it("Should throw an error, if user claiming return is not an investor in the property", async ()=>{
+            await expect((promissory.connect(promissoryUser3).claimReturn(1, 10))).to.be.revertedWith('You have not invested in this property!')
+        })
+        it("Should throw an error, investor is claiming more than expected", async ()=>{
+            await expect((promissory.connect(promissoryUser2).claimReturn(1, 50))).to.be.revertedWith('Amount exceeds than available!')
+        })
+        it("User should be able to claim, the investment partially", async ()=>{
+            await expect((promissory.connect(promissoryUser1).claimReturn(1,10))).to.be.emit(promissory, 'ClaimedReturn')
+        })
+        it("Investor should be able to claim investment successfully", async ()=>{
+            await expect((promissory.connect(promissoryUser1).claimReturn(1,20))).to.be.emit(promissory, 'ClaimedInvestment')
         })
     })
 })
